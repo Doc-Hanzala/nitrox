6a63d5edb87fcd7d791d7355054a520e31ea98c6
hanzala tanveer <810hanzalatanveer@gmail.com>
2024-06-05 16:17:11 -0700

    fixed a bug

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -48,7 +48,7 @@ const Navbar = () => {
               />
 
               <span className="hidden md:block text-white text-2xl font-bold ml-2">
-                PropertyPulse
+                Nitrox
               </span>
             </a>
             {/* <!-- Desktop Menu Hidden below md screens --> */}
