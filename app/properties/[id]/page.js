41f0322e668a343917f62901c5d413168c3b8850
hanzala tanveer <810hanzalatanveer@gmail.com>
2024-06-05 15:40:12 -0700

    first commit

---
+++ app/properties/[id]/page.js
@@ -0,0 +1,5 @@
+const DynamicPropertypage = () => {
+  return <div>DynamicPropertypage</div>;
+};
+
+export default DynamicPropertypage;
---
+++ app/properties/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const Propertiespage = () => {
+  return (
+    <div>
+      <h1>back home</h1>
+      <Link href="/">back to home</Link>
+    </div>
+  );
+};
+
+export default Propertiespage;
